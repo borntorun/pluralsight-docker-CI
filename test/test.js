491d78593b6fef020e85171e2a56a4a35adb2d55
João Carvalho <jmmtcarvalho@gmail.com>
2016-05-10 09:42:12 +0100

    altered description for test

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var request = require("supertest"),
     app = require("../app").getApp;
 
 describe('GET /', function(){
-  it('expects HTTP response 200', function(done){
+  it('expects HTTP response code 200', function(done){
     request(app)
      .get('/')
 	 .expect(200, done); 
